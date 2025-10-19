fd43b7338bba0ca7b68482a6eaa524a36886cc21
genuinelycool <genuinelycool@gmail.com>
2025-10-19 09:52:40 +0545

    make and structure ApiController and routes

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class ApiController extends Controller
+{
+    // Register API [name, email, profile_image, password, password_confirmation]
+    public function register() {
+
+    }
+
+    // Login API [email, password]
+    public function login() {
+
+    }
+
+    // Profile API
+    public function profile() {
+
+    }
+
+    // Refresh Token API
+    public function refreshToken() {
+
+    }
+
+    // Logout API
+    public function logout() {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::post('register', [ApiController::class, 'register']);
+Route::post('login', [ApiController::class, 'login']);
+
+Route::group(["middleware" => ["auth:api"]], function() {
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
