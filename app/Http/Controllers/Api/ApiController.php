c8fe73ac6cfed2f95a2d6dca147a0b8bf09190aa
genuinelycool <genuinelycool@gmail.com>
2025-10-20 17:40:41 +0545

    login with token generation

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
@@ -31,8 +32,35 @@ class ApiController extends Controller
     }
 
     // Login API [email, password]
-    public function login() {
+    public function login(Request $request) {
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
 
+        $user = User::where("email", $request->email)->first();
+
+        if (!empty($user)) {
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken("myToken")->accessToken;
+
+                return response()->json([
+                    "status" => true,
+                    "message" => "User logged in",
+                    "token" => $token
+                ]);
+            } else {
+                return response()->json([
+                    "status" => false,
+                    "message" => "Password did not match"
+                ]);
+            }
+        } else {
+            return response()->json([
+                "status" => false,
+                "message" => "Email not found"
+            ]);
+        }
     }
 
     // Profile API
