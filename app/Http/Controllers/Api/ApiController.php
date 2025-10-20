048409040c8392bf0de6fd7863363de3d7e56d3c
genuinelycool <genuinelycool@gmail.com>
2025-10-20 12:30:47 +0545

    register user with validation

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -9,8 +9,25 @@ use App\Models\User;
 class ApiController extends Controller
 {
     // Register API [name, email, profile_image, password, password_confirmation]
-    public function register() {
+    public function register(Request $request) {
+        $data =$request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|confirmed",
+            "profile_image" => "nullable|image"
+        ]);
 
+        // Check Image Available
+        if ($request->hasFile("profile_image")) {
+            $data["profile_image"] = $request->file("profile_image")->store("users", "public");
+        }
+
+        User::create($data);
+
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully"
+        ]);
     }
 
     // Login API [email, password]
